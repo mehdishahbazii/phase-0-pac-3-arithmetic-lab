1eff5043631b418187202d6fd920ed995d7665dc
mehdishahbazi <mehdishahbazii.ms@gmail.com>
2023-08-16 16:52:04 -0400

    Arithmetic lab

---
+++ index.js
@@ -0,0 +1,52 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add() {
+    return "something";
+}
+function add(a,b) {
+    return a + b;
+}
+function subtract() {
+    return "something";
+}
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply() {
+    return "something";
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide() {
+    return "something";
+}
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++
+    return n;
+}
+function decrement(n) {
+    n--
+    return n--;
+}
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
